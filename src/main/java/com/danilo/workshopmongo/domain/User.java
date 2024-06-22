57318a74c997e8a973543feae72a8797fca064bf
Danilo Brandão <danilo.brandao.ifpr@gmail.com>
2024-06-22 02:10:35 -0300

    Ref User Posts

--- src/main/java/com/danilo/workshopmongo/domain/User.java
+++ src/main/java/com/danilo/workshopmongo/domain/User.java
@@ -1,9 +1,12 @@
 package com.danilo.workshopmongo.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 //é possivel colocar apena o @Document, o spring data vai mapear a coleção com o mesmo nome da classe, mas com letra minuscula
@@ -16,6 +19,10 @@ public class User implements Serializable{
 	private String id;
 	private String name;
 	private String email;
+	
+//	(lazy = true) -> faz com que os posts sejam carregados apenas quando forem acessados
+	@DBRef(lazy = true)
+	private List<Post> posts = new ArrayList<>();
 	
 	public User() {}
 
@@ -48,6 +55,10 @@ public class User implements Serializable{
 
 	public void setEmail(String email) {
 		this.email = email;
+	}
+	
+	public List<Post> getPosts() {
+		return posts;
 	}
 
 	@Override
