609dfdefacff31d2dcf55ffa73be31424f61a1da
Danilo Brandão <danilo.brandao.ifpr@gmail.com>
2024-06-21 22:23:29 -0300

    Projection of author data with DTO

--- src/main/java/com/danilo/workshopmongo/domain/Post.java
+++ src/main/java/com/danilo/workshopmongo/domain/Post.java
@@ -7,6 +7,8 @@ import java.util.Objects;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import com.danilo.workshopmongo.dto.AuthorDTO;
+
 //Declara que a classe é um documento do mongoDB
 @Document
 public class Post implements Serializable {
@@ -17,11 +19,11 @@ public class Post implements Serializable {
 	private Date date;
 	private String title; 
 	private String body;
-	private User author; 
+	private AuthorDTO author; 
 	
 	public Post() {}
 
-	public Post(String id, Date date, String title, String body, User author) {
+	public Post(String id, Date date, String title, String body, AuthorDTO author) {
 		super();
 		this.id = id;
 		this.date = date;
@@ -62,19 +64,12 @@ public class Post implements Serializable {
 		this.body = body;
 	}
 
-	public User getUser() {
-		return author;
-	}
-
-	public void setUser(User user) {
-		this.author = user;
-	}
 	
-	public User getAuthor() {
+	public AuthorDTO getAuthor() {
 		return author;
 	}
 
-	public void setAuthor(User author) {
+	public void setAuthor(AuthorDTO author) {
 		this.author = author;
 	}
 	
