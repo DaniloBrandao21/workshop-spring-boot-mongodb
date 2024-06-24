92b84e9a85a56e1231ee4814e7c7ef68750f5b15
Danilo Brandão <danilo.brandao.ifpr@gmail.com>
2024-06-24 15:37:42 -0300

    Complex Query

--- src/main/java/com/danilo/workshopmongo/resource/PostResource.java
+++ src/main/java/com/danilo/workshopmongo/resource/PostResource.java
@@ -1,5 +1,6 @@
 package com.danilo.workshopmongo.resource;
 
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -27,11 +28,25 @@ public class PostResource {
 		return ResponseEntity.ok().body(post);
 	}
 	
-	@GetMapping(value = "/titleSearch")
+	@GetMapping(value = "/titlesearch")
 	public ResponseEntity<List<Post>> findByTitle(@RequestParam(value = "text", defaultValue = "") String text){
 		text = URL.decodeParam(text);
 		List<Post> postList = service.findByTitle(text);
 		return ResponseEntity.ok().body(postList);
 	}
+	
+	@GetMapping(value = "/fullsearch")
+	public ResponseEntity<List<Post>> fullsearch(
+			@RequestParam(value = "text", defaultValue = "") String text,
+			@RequestParam(value = "minDate", defaultValue = "") String minDate,
+			@RequestParam(value = "maxDate", defaultValue = "") String maxDate){
+		text = URL.decodeParam(text);
+		Date min = URL.convertDate(minDate, new Date(0L));
+		Date max = URL.convertDate(maxDate, new Date());
+		List<Post> postList = service.fullSearch(text, min, max);
+		return ResponseEntity.ok().body(postList);
+	}
+	
+	
 
 }
