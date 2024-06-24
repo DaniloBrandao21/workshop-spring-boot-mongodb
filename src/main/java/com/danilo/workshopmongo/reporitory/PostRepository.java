16b8562f5ee0e2fd6a416add9cb83c0b1def3b96
Danilo Brandão <danilo.brandao.ifpr@gmail.com>
2024-06-24 12:16:02 -0300

    Simple query with @Query

--- src/main/java/com/danilo/workshopmongo/reporitory/PostRepository.java
+++ src/main/java/com/danilo/workshopmongo/reporitory/PostRepository.java
@@ -3,12 +3,20 @@ package com.danilo.workshopmongo.reporitory;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.danilo.workshopmongo.domain.Post;
 
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
+	
+	
+	//Consulta simples utilizando o @Query[
+	//?0 para usar o primeiro parametro que veio no método  
+	//options 'i' para ignorar sensitiveCase
+	@Query("{'title': {$regex: ?0, $options: 'i'} }")
+	List<Post> searchTitle(String text);
 	
 	List<Post> findByTitleContainingIgnoreCase(String text);
 }
--- src/main/java/com/danilo/workshopmongo/service/PostService.java
+++ src/main/java/com/danilo/workshopmongo/service/PostService.java
@@ -21,9 +21,13 @@ public class PostService {
 	}
 	
 	public List<Post> findByTitle(String text){
-		return repository.findByTitleContainingIgnoreCase(text);
+		return repository.searchTitle(text);
 		
 	}
+	
+
+	
+	
 	
 
 }
